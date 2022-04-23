7321dbb52a9d30140e4153409b919897fcec9eed
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-04-23 22:40:37 +0200

    skifter i og j

--- intersect_cell.m
+++ intersect_cell.m
@@ -1,19 +1,20 @@
 function bool_intersect = intersect_cell(i,j,a,N,rho,theta)
-% Checks if the line l(rho,theta) passes through the cell C{ij}
-% input:
+% Checks if the line l(rho,theta) passes through the cell C_{ij}
+% inputs:
 %   i - first cell index
 %   j - first cell index
 %   a - parameter describing the size of the region B
 %   N - number of cells in a row of the region B
 %   rho - minimal distance between the origin and the ray
-%   theta - angle between the ray and tha y-axis
+%   theta - angle between the ray and the y-axis
 % output:
-%   bool_intersect = 1 if the ray passes through the cell C{ij}, 0
+%   bool_intersect = 1 if the ray passes through the cell C_{ij}, 0
 %   otherwise.
 
 bool_intersect = 0;
 
-[x0,y0,x1,y1] = deal(-a+(2*a/N)*(i-1), -a+(2*a/N)*(j-1), -a+(2*a/N)*(i), -a+(2*a/N)*(j)); % computes the corners of the cell
+% Compute the corners of the cell
+[x0,y0,x1,y1] = deal(-a+(2*a/N)*(j-1), -a+(2*a/N)*(i-1), -a+(2*a/N)*(j), -a+(2*a/N)*(i)); 
 
 % The first case is when theta is in [0,pi/2]
 if theta <= pi/2
