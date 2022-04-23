fcdae5f8204b678f4d5abe133da0c5c83ebf022b
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-04-23 22:43:08 +0200

    skiftede til metode hvor man kigger på alle muligheder

--- get_length.m
+++ get_length.m
@@ -1,34 +1,45 @@
-function length=get_length(i,j,a,N,rho,theta)
-    % The function finds the length of the line-segment of the line that
-    % Passes through the cell
-    % Usage: length=get_length(i,j,a,N,rho,theta)
-    % Inputs: i,j defines the cell number
-    %   a is the size of the square
-    %   N is the size of the partitions of the cell
-    %   rho,theta defines the values for the line equation
-    %   rho=x*cos(theta)+y*sin(theta)
+function sijk = get_length(i,j,a,N,rho,theta)
+% Calculates the length of the line segment of the ray l(rho,theta) lying
+% inside the cell C_{ij}.
+% inputs:
+%   i - first cell index
+%   j - first cell index
+%   a - parameter describing the size of the region B
+%   N - number of cells in a row of the region B
+%   rho - minimal distance between the origin and the ray
+%   theta - angle between the ray and the y-axis
+% output:
+%   sijk - the length s_{ij,k}
 
-    %Create the sides of the cell used
-    [x0,y0,x1,y1]=deal(-a+(2*a/N)*(i-1),-a+(2*a/N)*(j-1),-a+(2*a/N)*(i),-a+(2*a/N)*(j));
-    % We have two special cases if the line is horizontal or vertical
-    % Here the lines are simply the paratition length i.e x1-x0=y1-y0
-    if sin(theta)==0 || cos(theta)==0
-        length=x1-x0;
+% Compute the corners of the cell
+[x0,y0,x1,y1] = deal(-a+(2*a/N)*(i-1),-a+(2*a/N)*(j-1),-a+(2*a/N)*(i),-a+(2*a/N)*(j));
+
+% For vertical and horizontal lines, the length equals the side of the cell
+if theta == 0 || (theta == pi || theta == pi/2)
+    sijk = 2*a/N;
+else
+    Points = zeros(2); % Points will store the coordinates of the points where the line enters and exits the cell
+    liney0 = (rho-x0*cos(theta))/sin(theta); % y-coordinate of the line at x = x0
+    if liney0 < y0
+        Points(1,2) = y0;
+        Points(1,1) = (rho-y0*sin(theta))/cos(theta);
+    elseif liney0 > y1
+        Points(1,2) = y1;
+        Points(1,1) = (rho-y1*sin(theta))/cos(theta);
     else
-        %The general case: We define the line equations for each side as
-        %(y1-y2)x+(x2-x1)y+(x1y2-x2y1)=0 and write it as a the matrix Ax=B
-        eq_Line=[cos(theta),sin(theta),rho];
-        eq_Sides=[0,x1-x0,x1*y1-x0*y1;0,x1-x0,x1*y0-x0*y0;y1-y0,0,x0*y0-x0*y1;y1-y0,0,x1*y0-x1*y1];
-        for i=1:4
-            %We solve for the intersection between the line and the sides
-            %and append them to a Matrix
-            X(:,i)=intersect_points(eq_Line,eq_Sides(i,:));
-        end
-        % We choose the indicies where the intersection is in the cell
-        % range
-        I=(find((x0<=X(1,:) & X(1,:)<=x1 & y0<=X(2,:) & X(2,:)<=y1)));
-        K=X(:,I);
-        %We get the length
-        length=norm(K(:,2)-K(:,1));
+        Points(1,:) = [x0;liney0];
     end
-end    
+
+    liney1 = (rho-x1*cos(theta))/sin(theta);
+    if liney1 < y0
+        Points(2,2) = y0;
+        Points(2,1) = (rho-y0*sin(theta))/cos(theta);
+    elseif liney1 > y1
+        Points(2,2) = y1;
+        Points(2,1) = (rho-y1*sin(theta))/cos(theta);
+    else
+        Points(2,:) = [x1;liney1];
+    end
+
+    sijk = abs(norm(Points(1,:)-Points(2,:)));
+end
